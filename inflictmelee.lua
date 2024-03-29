89c3b07a86b82268d4add84d78a04d54e5ed5810
DulledDarkness <159194225+DulledDarkness@users.noreply.github.com>
2024-03-29 19:53:10 -0400

    Update inflictmelee.lua

--- inflictmelee.lua
+++ inflictmelee.lua
@@ -1,32 +1,34 @@
 local Players = game:GetService("Players")
 local Player = Players.LocalPlayer
 
-local TestPName = "some_mobile"
-local TestP:Player = Players:FindFirstChild(TestPName)
+local Character = Player.Character or Player.CharacterAdded:Wait()
 
-local Character = TestP.Character or TestP.CharacterAdded:Wait() or Player.Character or Player.CharacterAdded:Wait()
-local Tool = Character:FindFirstChildWhichIsA("Tool")
+for i, v in pairs(Players:GetChildren()) do 
+	if v.Name ~= tostring(Player.Name) and v.Character then
+		local Char = v.Character
+		local Tool = Character:FindFirstChildWhichIsA("Tool")
 
-if Tool ~= nil then
-	local ModuleSetting = Tool:FindFirstChild("Setting")
-	if ModuleSetting then
-		local RequiredModule = require(ModuleSetting)
-		local GunScript_Server = Tool:FindFirstChild("GunScript_Server")
-		if GunScript_Server then
-			local InflictMelee:RemoteFunction = GunScript_Server.InflictTargetMelee or GunScript_Server:WaitForChild("InflictTargetMelee")
-			
-			local THumanoid = Character:FindFirstChildWhichIsA("Humanoid")
-			local TTorso = Character.Torso or Character:WaitForChild("Torso")
-			
-			local A1 = {RequiredModule.MeleeDamage, RequiredModule.MeleeHeadshotEnabled, RequiredModule.MeleeHeadshotDamageMultiplier}
-			local A2_1 = {RequiredModule.MeleeKnockback, RequiredModule.MeleeLifesteal, RequiredModule.MeleeAffliction, RequiredModule.MeleeAfflictionName, RequiredModule.MeleeAfflictChance}
-			local A2_2 = {RequiredModule.MeleeKnockback, RequiredModule.MeleeLifesteal, RequiredModule.MeleeAffliction, "IcifyScript", RequiredModule.MeleeAfflictChance}
-			local A3 = {RequiredModule.MeleeCriticalDamageEnabled, RequiredModule.MeleeCriticalBaseChance, RequiredModule.MeleeCriticalDamageMultiplier}
-			local Hit = TTorso
-						
-			coroutine.resume(coroutine.create((function()
-				InflictMelee:InvokeServer(RequiredModule, THumanoid, TTorso, A1, A2_2, A3, Hit)
-			end)))
-		end
+		if Tool ~= nil then
+			local ModuleSetting = Tool:FindFirstChild("Setting")
+			if ModuleSetting then
+				local RequiredModule = require(ModuleSetting)
+				local GunScript_Server = Tool:FindFirstChild("GunScript_Server")
+				if GunScript_Server then
+					local InflictMelee:RemoteFunction = GunScript_Server.InflictTargetMelee or GunScript_Server:WaitForChild("InflictTargetMelee")
+					
+					local THumanoid = Char:FindFirstChildWhichIsA("Humanoid")
+					local TTorso = Char.Torso or Char:WaitForChild("Torso")
+					
+					local A1 = {RequiredModule.MeleeDamage, RequiredModule.MeleeHeadshotEnabled, RequiredModule.MeleeHeadshotDamageMultiplier}
+					local A2_1 = {RequiredModule.MeleeKnockback, RequiredModule.MeleeLifesteal, RequiredModule.MeleeAffliction, RequiredModule.MeleeAfflictionName, RequiredModule.MeleeAfflictChance}
+					local A2_2 = {RequiredModule.MeleeKnockback, RequiredModule.MeleeLifesteal, true, "IcifyScript", RequiredModule.MeleeAfflictChance}
+					local A3 = {RequiredModule.MeleeCriticalDamageEnabled, RequiredModule.MeleeCriticalBaseChance, RequiredModule.MeleeCriticalDamageMultiplier}
+					local Hit = TTorso
+
+					InflictMelee:InvokeServer(RequiredModule, THumanoid, TTorso, A1, A2_2, A3, Hit)
+					print("Fired on player: "..tostring(v.Name))
+				end
+			end
+		end		
 	end
 end
